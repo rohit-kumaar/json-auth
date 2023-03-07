a77d282782e8724de566627ce0bcc98db12edfb4
Rohit Kumar <rohit96682@gmail.com>
2023-03-07 11:55:19 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,56 +1,40 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
-import { addEmployees } from "../services/employeesService";
 import { Link, useNavigate } from "react-router-dom";
-import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-function Registration() {
+function Login() {
   const navigate = useNavigate();
 
   const postData = (values) => {
-    addEmployees(values).then((res) => {
-      alert("Registration successfully done");
-      navigate("/login");
-    });
+    console.log(values.email, values.password);
   };
 
   const validationSchema = Yup.object({
-    name: Yup.string()
-      .min(5, "Must be 5 characters or greater")
-      .required("Please enter a full name"),
-    email: Yup.string().required("Please enter an email id"),
-    password: Yup.string().required("Please enter a password"),
+    email: Yup.string()
+      .min(5, "Must be include @")
+      .required("Please enter a correct email id."),
+    password: Yup.string().required("Please enter a correct password."),
   });
 
   return (
     <>
       <Formik
         initialValues={{
-          name: "",
           email: "",
           password: "",
         }}
         validationSchema={validationSchema}
         onSubmit={(values) => postData(values)}
       >
-        <Form className="mt-5 ">
+        <Form className="mt-5">
           <div className="container w-50">
-            <h1 className="text-center mb-4">Registration</h1>
+            <h1 className="text-center">Login</h1>
 
             <div className="row">
               <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                  <b>Full Name</b>
-                </label>
-                <Field type="text" className="form-control" name="name" />
-                <div className="text-danger">
-                  <ErrorMessage name="name" />
-                </div>
-              </div>
-
-              <div className="mb-3">
                 <label htmlFor="email" className="form-label">
-                  <b>Email ID</b>
+                  <b>Employee email</b>
                 </label>
                 <Field type="email" className="form-control" name="email" />
                 <div className="text-danger">
@@ -60,7 +44,7 @@ function Registration() {
 
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">
-                  <b>Password</b>
+                  <b>Employee password</b>
                 </label>
                 <Field
                   type="password"
@@ -75,11 +59,11 @@ function Registration() {
 
             <div className="d-flex align-content-center gap-3 mt-3">
               <button type="submit" className="btn btn-primary">
-                Submit
+                Login
               </button>
 
-              <Link to="/login" className="align-self-end">
-                Already Register
+              <Link to="/" className="align-self-end">
+                New Registration
               </Link>
             </div>
           </div>
@@ -89,4 +73,4 @@ function Registration() {
   );
 }
 
-export default Registration;
+export default Login;
