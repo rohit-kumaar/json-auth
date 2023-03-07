59ccd8b67a3a02e2e497ab0cc174547600b2d062
Rohit Kumar <rohit96682@gmail.com>
2023-03-07 11:58:40 +0530

    final : ui

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <nav className="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Profile;
